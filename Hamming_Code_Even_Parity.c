eb674386dbbfbf9c9507eb2301da2109eb4e186b
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-03-05 14:57:13 +0600

    Add files via upload

---
+++ Hamming_Code_Even_Parity.c
@@ -0,0 +1,63 @@
+*/ 
+
+Author - Shamura Ahmad 
+
+*/
+
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <conio.h>  
+#include <math.h>  
+#include <string.h>
+
+void reverse ( char ar [] , int n ){  
+  int i,tmp ;  
+  for( i = 1 ; i < n/2 ; i++ ){  
+    tmp = ar[i] ;  
+    ar[i] = ar[n-i] ;  
+    ar[n-i] = tmp ;  
+  }  
+}  
+int length(char ar[]){
+   int l =0;
+   while(ar[l]!='\0') l++;
+   return l;
+}
+
+void hamming (char ham[], int n){
+    int k=0,c,i,j,m;
+    i=pow(2,k);
+    while(i < n){
+        c=0;
+        for(j = i; j < n; j++) {if(((j-i)/i)%2==0 && ham[j]=='1') c++;}
+        if(c%2==1) ham[i]='1';
+        else ham[i]='0';
+        k++;
+        i=pow(2,k);
+    }
+    }
+int main() {
+    char ar [100];
+    printf("Enter data string : ");
+    scanf("%s",&ar);
+    int k = length(ar)-1;
+    char ham [100];
+    int i,l;
+    for(i = 1; k >= 0; i++){
+        l=log2(i);
+        if(log2(i)-l==0) ham[i]='X';
+        else ham[i]=ar[k--];
+    }
+   
+    l=i;
+    reverse(ham,l);
+    printf("Before hamming detection : ");
+    for(int i = 1;i < l; i++) printf("%c ",ham[i]);
+    printf("\n");
+    reverse(ham,l);
+    hamming(ham,l);
+    reverse(ham,l);
+    printf("After hamming detection  : ");
+    for(int i = 1;i < l; i++) printf("%c ",ham[i]);
+    return 0;
+}
