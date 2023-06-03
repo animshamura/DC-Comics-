20930f72f3b58e509a139163a4434b838418c476
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-06-03 12:10:53 +0600

    Create Pseudo Ternary.c

---
+++ Pseudo Ternary.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int t;
+    printf("Enter the length of the data stream : ");
+    scanf("%d",&t);
+    int ar[t];
+    printf("Enter the data stream : ");
+    for(int i = 0; i < t; i++) scanf("%d",&ar[i]);
+    int p = 1;
+    char pos [] = {'N','P'};
+    printf("Output signal :  ");
+    for(int i= 0; i < t; i++){
+    if(ar[i]==1) printf("Z ");
+    else {printf("%c ",pos[p]); p=!p;}
+    }
+    printf("\n");
+    return 0;
+}
